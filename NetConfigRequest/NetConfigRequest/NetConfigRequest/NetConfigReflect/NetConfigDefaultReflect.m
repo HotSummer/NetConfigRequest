739b54bfa6b8d97130117e7fc2023b338cbb52e4
summer.zhu <summer.zhu@vipshop.com>
2015-01-27 14:13:58 +0800

    网络配置修改

--- NetConfigRequest/NetConfigRequest/NetConfigRequest/NetConfigReflect/NetConfigDefaultReflect.m
+++ NetConfigRequest/NetConfigRequest/NetConfigRequest/NetConfigReflect/NetConfigDefaultReflect.m
@@ -12,6 +12,29 @@
 @implementation NetConfigDefaultReflect
 
 - (NSDictionary *)requestDataFromConfig:(NetConfigModel *)configModel requestObject:(NSObject *)requestObj{
+    NSMutableDictionary *paramers = [NSMutableDictionary dictionary];
+    [configModel.reqParam enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        NSArray *array = [(NSString*)obj componentsSeparatedByString:@"."];
+        if (array.count == 1) {
+            [paramers setObject:array[0] forKey:key];
+        }else{
+            NSString *strClass = array[0];
+            NSObject *object;
+            NSString *requestClassName = NSStringFromClass([requestObj class]);
+            //配制的类名和对象的类名一致,则从该对象中获取值
+            if ([strClass isEqualToString:requestClassName]) {
+                object = requestObj;
+            }else{//不一致的话，调用shareInstance方法创建该对象
+                Class class = NSClassFromString(strClass);
+                NSObject *obj = (NSObject *)class;
+                if ([obj respondsToSelector:@selector(shareInstance)]) {
+                    object = [obj performSelector:@selector(shareInstance)];
+                }
+                 
+            }
+        }
+    }];
+    
     return nil;
 }
 
